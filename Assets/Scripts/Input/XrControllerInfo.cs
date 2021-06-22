78eb690b7f69708466aa8f61ff89c30906c0e4b9
Bill Quith <chinbillybilbo@gmail.com>
2021-06-22 23:04:30 +0100

    Fixed some controller mappings. - Can draw now, but if tool palette used we stop drawing.

--- Assets/Scripts/Input/XrControllerInfo.cs
+++ Assets/Scripts/Input/XrControllerInfo.cs
@@ -18,10 +18,14 @@ using UnityEngine.XR;
 
 namespace TiltBrush
 {
+
+    // Unity XR Controller.
+    // - https://docs.unity3d.com/2019.4/Documentation/ScriptReference/XR.CommonUsages.html
     public class XrControllerInfo : ControllerInfo
     {
         private InputDeviceCharacteristics m_characteristics;
         private InputDevice m_device;
+        private uint m_currInputState = 0u, m_prevInputState = 0u;
 
         public override bool IsTrackedObjectValid { get; set; }
 
@@ -74,6 +78,9 @@ namespace TiltBrush
         {
             base.Update();
             UpdatePoses();
+
+            m_prevInputState = m_currInputState;
+            m_currInputState = 0u;
         }
 
         public override float GetTriggerRatio()
@@ -89,13 +96,14 @@ namespace TiltBrush
 
         public override Vector2 GetPadValue()
         {
-            Vector2 value;
-            if (m_device.isValid && m_device.TryGetFeatureValue(CommonUsages.secondary2DAxis, out value))
-            {
-                return value;
-            }
+            return GetThumbStickValue();
 
-            return Vector2.zero;
+            // Vector2 value;
+            // if (m_device.isValid && m_device.TryGetFeatureValue(CommonUsages.secondary2DAxis, out value))
+            // {
+            //     return value;
+            // }
+            // return Vector2.zero;
         }
 
         public override Vector2 GetThumbStickValue()
@@ -111,7 +119,15 @@ namespace TiltBrush
 
         public override Vector2 GetPadValueDelta()
         {
-            return Vector2.zero;
+            return new Vector2(GetScrollXDelta(), GetScrollYDelta());
+
+            // Vector2 value;
+            // if (m_device.isValid && m_device.TryGetFeatureValue(CommonUsages.secondary2DAxis, out value))
+            // {
+            //     return value;
+            // }
+            //
+            // return Vector2.zero;
         }
 
         public override float GetGripValue()
@@ -156,139 +172,117 @@ namespace TiltBrush
             return 0.0f;
         }
 
-        private bool CheckVrTouch(VrInput input)
-        {
-            bool value = false;
-            switch (input)
-            {
-                case VrInput.Button01:
-                case VrInput.Button04:
-                case VrInput.Button06:
-                    if (m_device.TryGetFeatureValue(CommonUsages.primaryButton, out value))
-                    {
-                        return value;
-                    }
-                    break;
-                case VrInput.Button02:
-                case VrInput.Button03:
-                case VrInput.Button05:
-                    if (m_device.TryGetFeatureValue(CommonUsages.secondaryButton, out value))
-                    {
-                        return value;
-                    }
-                    break;
-                case VrInput.Directional:
-                case VrInput.Thumbstick:
-                case VrInput.Touchpad:
-                    if (m_device.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out value))
-                    {
-                        return value;
-                    }
-                    break;
-                case VrInput.Any:
-                    // return  OVRInput.Touch.One |
-                    //         OVRInput.Touch.Two |
-                    //         OVRInput.Touch.PrimaryThumbstick;
-                    if (m_device.TryGetFeatureValue(CommonUsages.primaryButton, out value) && value)
-                        return true;
-                    if (m_device.TryGetFeatureValue(CommonUsages.secondaryButton, out value) && value)
-                        return true;
-                    if (m_device.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out value) && value)
-                        return true;
-                    break;
-                default:
-                    Debug.Assert(false, $"Invalid touch button enum: {input.ToString()}");
-                    break;
-            }
-
-            return value;
-        }
-
         public override bool GetVrInputTouch(VrInput input)
         {
+            bool value = false;
             if (m_device.isValid)
             {
-                return CheckVrTouch(input);
+                switch (input)
+                {
+                    case VrInput.Button01:
+                    case VrInput.Button04:
+                    case VrInput.Button06:
+                        if (m_device.TryGetFeatureValue(CommonUsages.primaryTouch, out value))
+                        {
+                            return value;
+                        }
+                        break;
+                    case VrInput.Button02:
+                    case VrInput.Button03:
+                    case VrInput.Button05:
+                        if (m_device.TryGetFeatureValue(CommonUsages.secondaryTouch, out value))
+                        {
+                            return value;
+                        }
+                        break;
+                    case VrInput.Directional:
+                    case VrInput.Thumbstick:
+                    case VrInput.Touchpad:
+                        if (m_device.TryGetFeatureValue(CommonUsages.primary2DAxisTouch, out value))
+                        {
+                            return value;
+                        }
+                        break;
+                    case VrInput.Any:
+                        if (m_device.TryGetFeatureValue(CommonUsages.primaryTouch, out value) && value)
+                            return true;
+                        if (m_device.TryGetFeatureValue(CommonUsages.secondaryTouch, out value) && value)
+                            return true;
+                        if (m_device.TryGetFeatureValue(CommonUsages.primary2DAxisTouch, out value) && value)
+                            return true;
+                        break;
+                    default:
+                        Debug.Assert(false, $"Invalid touch button enum: {input.ToString()}");
+                        break;
+                }
+
             }
 
             return false;
         }
 
-        private bool CheckVrInput(VrInput input)
-        {
-            bool value = false;
-            switch (input)
-            {
-                case VrInput.Directional:
-                case VrInput.Thumbstick:
-                case VrInput.Touchpad:
-                    if (m_device.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out value))
-                    {
-                        return value;
-                    }
-                    break;
-
-                case VrInput.Trigger:
-                    if (m_device.TryGetFeatureValue(CommonUsages.triggerButton, out value))
-                    {
-                        return value;
-                    }
-                    break;
-
-                case VrInput.Grip:
-                    if (m_device.TryGetFeatureValue(CommonUsages.gripButton, out value))
-                    {
-                        return value;
-                    }
-                    break;
-
-                case VrInput.Button04:
-                case VrInput.Button01:
-                case VrInput.Button06:
-                    // Pad_Left, Pad_Down, Full pad, (X,A)
-                    if (m_device.TryGetFeatureValue(CommonUsages.primaryButton, out value))
-                    {
-                        return value;
-                    }
-                    break;
-
-                case VrInput.Button02:
-                case VrInput.Button03:
-                case VrInput.Button05:
-                    // Pad_Right, Pad_Up, Application button, (Y,B)
-                    if (m_device.TryGetFeatureValue(CommonUsages.secondaryButton, out value))
-                    {
-                        return value;
-                    }
-                    break;
-
-                case VrInput.Any:
-                    //     return OVRInput.Button.One
-                    //         | OVRInput.Button.Two
-                    //         | OVRInput.Button.PrimaryThumbstick
-                    //         | OVRInput.Button.PrimaryIndexTrigger
-                    //         | OVRInput.Button.PrimaryHandTrigger
-                    if (m_device.TryGetFeatureValue(CommonUsages.primaryButton, out value) && value)
-                        return true;
-                    if (m_device.TryGetFeatureValue(CommonUsages.secondaryButton, out value) && value)
-                        return true;
-                    if (m_device.TryGetFeatureValue(CommonUsages.triggerButton, out value) && value)
-                        return true;
-                    if (m_device.TryGetFeatureValue(CommonUsages.gripButton, out value) && value)
-                        return true;
-                    if (m_device.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out value) && value)
-                        return true;
-                    break;
-            }
-
-            return value; // Should never get here.
-        }
-
         public override bool GetVrInput(VrInput input)
         {
+            bool value = false;
+
             if (m_device.isValid)
             {
-                return CheckVrInput(input);
+                switch (input)
+                {
+                    case VrInput.Directional:
+                    case VrInput.Thumbstick:
+                    case VrInput.Touchpad:
+                        if (m_device.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out value))
+                        {
+                            return value;
+                        }
+                        break;
+
+                    case VrInput.Trigger:
+                        if (m_device.TryGetFeatureValue(CommonUsages.triggerButton, out value))
+                        {
+                            return value;
+                        }
+                        break;
+
+                    case VrInput.Grip:
+                        if (m_device.TryGetFeatureValue(CommonUsages.gripButton, out value))
+                        {
+                            return value;
+                        }
+                        break;
+
+                    case VrInput.Button01:
+                    case VrInput.Button04:
+                    case VrInput.Button06:
+                        if (m_device.TryGetFeatureValue(CommonUsages.primaryButton, out value))
+                        {
+                            return value;
+                        }
+                        break;
+
+                    case VrInput.Button02:
+                    case VrInput.Button03:
+                    case VrInput.Button05:
+                        if (m_device.TryGetFeatureValue(CommonUsages.secondaryButton, out value))
+                        {
+                            return value;
+                        }
+                        break;
+
+                    case VrInput.Any:
+                        if (m_device.TryGetFeatureValue(CommonUsages.primaryButton, out value) && value)
+                            return true;
+                        if (m_device.TryGetFeatureValue(CommonUsages.secondaryButton, out value) && value)
+                            return true;
+                        if (m_device.TryGetFeatureValue(CommonUsages.triggerButton, out value) && value)
+                            return true;
+                        if (m_device.TryGetFeatureValue(CommonUsages.gripButton, out value) && value)
+                            return true;
+                        if (m_device.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out value) && value)
+                            return true;
+                        break;
+                }
             }
 
             return false;
@@ -296,9 +290,12 @@ namespace TiltBrush
 
         public override bool GetVrInputDown(VrInput input)
         {
-            if (m_device.isValid)
+            uint flag = 1u << (int)input;
+
+            if (GetVrInput(input))
             {
-                return CheckVrInput(input);
+                m_currInputState |= flag;
+                return (m_prevInputState & flag) == 0;
             }
 
             return false;
