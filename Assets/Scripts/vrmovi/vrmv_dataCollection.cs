4f774b862ee324c940e615bd9ca677b22b87f345
trudiQ <agivenchy@hotmail.com>
2022-07-06 19:00:39 -0700

    added fbx export.

---
+++ Assets/Scripts/vrmovi/vrmv_dataCollection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TiltBrush;
+
+public class vrmv_dataCollection : MonoBehaviour
+{
+    // member variables
+    bool b_exported;
+    string fbx_filePath;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        b_exported = false;
+        fbx_filePath = Application.dataPath + "/Models/vrmovi/" + "test.fbx";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !b_exported)
+        {
+            ExportFbx.Export(fbx_filePath, ExportFbx.kFbxBinary);
+            b_exported = true;
+            Debug.Log("vrmv_dataCollection: Sketch model exported!");
+        }
+    }
+}
